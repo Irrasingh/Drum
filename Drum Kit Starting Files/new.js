022efed992c86b6be8cd6bd38902c42c5d8204ab
Ira Singh <90980060+Irrasingh@users.noreply.github.com>
2024-03-12 10:54:00 +0530

    added the all files
    please beaware with the changes

---
+++ Drum Kit Starting Files/new.js
@@ -0,0 +1,53 @@
+
+ /*   var buttonInnerHTML = this.innerHTML;
+    switch (buttonInnerHTML) {
+        case "w":
+            var tom1 =new Audio("sounds/crash.mp3");
+            tom1.play();
+            break;
+
+
+            case "a":
+            var tom2 =new Audio("sounds/kick-bass.mp3");
+            tom2.play();
+            break;
+
+            case "s":
+            var tom3 =new Audio("sounds/snare.mp3");
+            tom3.play();
+            break;
+
+            case "d":
+            var tom4 =new Audio("sounds/crash.mp3");
+            tom4.play();
+
+            case "j":
+            var tom45 =new Audio("sounds/crash.mp3");
+            tom45.play();
+            break;
+        
+
+            case "k":
+            var tom5 =new Audio("sounds/crash.mp3");
+            tom5.play();
+            break;
+
+            case "l":
+            var tom6 =new Audio("sounds/crash.mp3");
+            tom6.play();
+            break;
+
+        default:console.log(buttonInnerHTML)
+
+            break;
+    }
+ /*
+
+
+
+ /alert("I got clicked");  
+
+  //let audio =new Audio("sounds/tom-1.mp3");
+ //audio.play();
+
+ //this.style.color="white";
